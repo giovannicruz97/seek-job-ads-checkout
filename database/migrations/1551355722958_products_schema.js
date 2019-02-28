9f31082e7f8ed88f632a87298b9d2a19feeb906f
Giovanni Cruz <giovanni.cruz97@hotmail.com>
2019-02-28 09:54:41 -0300

    Update new migrations

---
+++ database/migrations/1551355722958_products_schema.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use("Schema");
+
+class ProductsSchema extends Schema {
+  up() {
+    this.create("products", table => {
+      table
+        .string("id")
+        .unique()
+        .notNullable();
+      table.string("name").notNullable();
+      table.decimal("price", 10, 2).notNullable();
+    });
+  }
+
+  down() {
+    this.drop("products");
+  }
+}
+
+module.exports = ProductsSchema;
---
+++ database/migrations/1551356641756_deals_schema.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use("Schema");
+
+class DealsSchema extends Schema {
+  up() {
+    this.create("deals", table => {
+      table.increments("id");
+      table
+        .string("product_id")
+        .references("id")
+        .inTable("products");
+      table
+        .integer("customer_id")
+        .references("id")
+        .inTable("customers");
+      table.integer("gets").notNullable();
+      table.integer("for").notNullable();
+    });
+  }
+
+  down() {
+    this.drop("deals");
+  }
+}
+
+module.exports = DealsSchema;
