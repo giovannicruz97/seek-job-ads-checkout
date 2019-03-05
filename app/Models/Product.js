52823a4830fff53d1a03e531a0266c4919ae0a80
Giovanni Cruz <giovanni.cruz97@hotmail.com>
2019-03-05 11:26:30 -0300

    Update Product style

--- app/Models/Product.js
+++ app/Models/Product.js
@@ -1,14 +1,14 @@
-"use strict";
+'use strict';
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use("Model");
+const Model = use('Model');
 
 class Product extends Model {
   discounts() {
-    return this.hasMany("App/Models/Discount");
+    return this.hasMany('App/Models/Discount');
   }
   deals() {
-    return this.hasMany("App/Models/Deal");
+    return this.hasMany('App/Models/Deal');
   }
 }
 
