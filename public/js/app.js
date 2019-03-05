b851c5a0a5aef495847e9a0a432df0950b26deac
Giovanni Cruz <giovanni.cruz97@hotmail.com>
2019-03-05 11:28:50 -0300

    Add  validation js to frontend

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
 function checkSelectedProducts() {
-  let classic = document.getElementById("classic").value;
-  let standout = document.getElementById("standout").value;
-  let premium = document.getElementById("premium").value;
+  let products = [...document.getElementsByTagName('input')];
+  delete products[0];
+  let filtered = products.filter(product => product.value > 0);
 
-  if (classic <= 0 && standout <= 0 && premium <= 0) {
-    alert("Select at least one product");
+  if (filtered.length <= 0) {
+    alert('Select at least one product');
     return false;
   }
 }
